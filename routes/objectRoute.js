e070fcf31a7f47f2387a55ed8c2ce09e38d87142
Naufal-Duta <naufaldutam@gmail.com>
2023-12-09 15:54:42 +0700

    remove auth from getAllObject

--- routes/objectRoute.js
+++ routes/objectRoute.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/add', addObject);
 
-router.get('/', auth, getAllObject);
+router.get('/', getAllObject);
 
 router.get('/list/:_id', auth, getObjectById);
 
