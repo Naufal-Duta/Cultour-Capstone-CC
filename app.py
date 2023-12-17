bed4089eb18117d047733237043a89fda8c8ae49
Naufal-Duta <naufaldutam@gmail.com>
2023-12-18 02:59:39 +0700

    testing deploy

--- app.py
+++ app.py
@@ -205,4 +205,6 @@ def nearest_restaurant():
             })
     
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
